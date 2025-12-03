96c5a2fbf6c9424dc562ac846dc0490a4c7245e1
thatonceagainkid <clayton.jeremy.lin@gmail.com>
2025-12-03 12:42:11 +0800

    Update smiley.py

--- smiley.py
+++ smiley.py
@@ -6,24 +6,32 @@ class Smiley:
     GREEN = (0, 255, 0)
     RED = (255, 0, 0)
     YELLOW = (255, 255, 0)
+    BLUE = (0, 0, 255)
     BLANK = (0, 0, 0)
 
-    def __init__(self):
+    def __init__(self, complexion=None):
         # We have encapsulated the SenseHat object
         self.sense_hat = SenseHat()
-
-        Y = self.YELLOW
+        
+        # Store complexion as an instance variable
+        self.my_complexion = complexion if complexion is not None else self.YELLOW
+        
+        X = self.complexion()
         O = self.BLANK
         self.pixels = [
-            O, Y, Y, Y, Y, Y, Y, O,
-            Y, Y, Y, Y, Y, Y, Y, Y,
-            Y, Y, Y, Y, Y, Y, Y, Y,
-            Y, Y, Y, Y, Y, Y, Y, Y,
-            Y, Y, Y, Y, Y, Y, Y, Y,
-            Y, Y, Y, Y, Y, Y, Y, Y,
-            Y, Y, Y, Y, Y, Y, Y, Y,
-            O, Y, Y, Y, Y, Y, Y, O,
+            O, X, X, X, X, X, X, O,
+            X, X, X, X, X, X, X, X,
+            X, X, X, X, X, X, X, X,
+            X, X, X, X, X, X, X, X,
+            X, X, X, X, X, X, X, X,
+            X, X, X, X, X, X, X, X,
+            X, X, X, X, X, X, X, X,
+            O, X, X, X, X, X, X, O,
         ]
+   
+    def complexion(self):
+        """Return the default complexion (color) of the smiley."""
+        return self.my_complexion
 
     def dim_display(self, dimmed=True):
         """
@@ -37,3 +45,5 @@ class Smiley:
         Show the smiley on the screen.
         """
         self.sense_hat.set_pixels(self.pixels)
+
+
